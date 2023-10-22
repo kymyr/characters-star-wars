a707ebd5e1df6e44906ab09f5972035dbba13f40
kymyr <kyl.tolentino@hoolisoftware.com>
2023-10-22 23:38:49 +0800

    to-do

--- tests/CharacterList.test.js
+++ tests/CharacterList.test.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import CharacterList from '../src/components/CharacterList';
-
-test('renders Star Wars Characters heading', () => {
-  render(<CharacterList />);
-  const headingElement = screen.getByText(/Star Wars Characters/i);
-  expect(headingElement).toBeInTheDocument();
-});
-
+// TODO: Test
