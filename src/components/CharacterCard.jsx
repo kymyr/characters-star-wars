0b917eddcdaa1531a92e75b1cdee0444b0ae7153
kymyr <kyl.tolentino@hoolisoftware.com>
2023-10-26 02:25:03 +0800

    added documentation

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 
-
+// Functional Component for displaying a Character Card
 const CharacterCard = ({ character, handleFavoriteToggle, favorites }) => {
+  // Check if the character is in favorites list
   const isFavorite = favorites.includes(character.name);
 
   return (
     <div className="card-area">
+      {/* Display the character image */}
       <img src={character.image} className="card-img-top" alt={character.name} />
       <div className="card-body">
+        {/* Display character details */}
         <p className="card-title">{character.name}</p>
         <p className="card-text">Height: {character.height} cm</p>
         <p className="card-text">Gender: {character.gender}</p>
         <p className="card-text">Birth Year: {character.birth_year}</p>
       </div>
       <div className="favorite-panel">
+        {/* Toggle favorite status */}
         <button
           className={`favorite-button ${isFavorite ? 'favorite' : ''}`}
           onClick={() => handleFavoriteToggle(character.name)}
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,9 +3,11 @@ import axios from 'axios';
 import CharacterCard from './CharacterCard';
 
 const CharacterList = () => {
+  // State variables for characters and favorites
   const [characters, setCharacters] = useState([]);
   const [favorites, setFavorites] = useState([]);
 
+  // Function to handle favorite toggling
   const handleFavoriteToggle = (character) => {
     setFavorites(prevFavorites => {
       if (prevFavorites.includes(character)) {
@@ -16,14 +18,16 @@ const CharacterList = () => {
     });
   };
 
-  
+  // Sorting characters based on favorites
   const sortedCharacters = characters.sort((a, b) =>
     favorites.includes(b.name) - favorites.includes(a.name)
   );
 
+  // Fetching characters from the backend
   useEffect(() => {
-    //  Local deploy
+    // Local deploy
     // axios.get('http://localhost:3001/api/characters')
+    
     // Deployed with render.com
     axios.get('https://star-wars-backend-5dv2.onrender.com/api/characters')
       .then(response => {
@@ -37,9 +41,12 @@ const CharacterList = () => {
   return (
     <div>
       <h1>Star Wars Characters</h1>
+
+      {/* List of Characters */}
       <div className="row">
         {sortedCharacters.map(character => (
           <div className="card" key={character.name}>
+            {/* Character Card Component */}
             <CharacterCard 
               character={character}
               handleFavoriteToggle={handleFavoriteToggle}
